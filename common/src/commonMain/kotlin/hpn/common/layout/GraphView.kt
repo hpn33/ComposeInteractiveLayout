574a936d67ba4b9c3efe831f98f687e318ac2b79
hamed <hamed.hp332@gmail.com>
2023-05-02 21:58:08 +0430

    [CLEAN] calculate by layers

--- common/src/commonMain/kotlin/hpn/common/layout/GraphView.kt
+++ common/src/commonMain/kotlin/hpn/common/layout/GraphView.kt
@@ -3,7 +3,6 @@ package hpn.common.layout
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.gestures.detectTransformGestures
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.onClick
@@ -35,16 +34,32 @@ import hpn.common.onMouseScroll
 
 /*
 
+# target
+- [ ] simulate geometry
+    - [ ] world
+    - [ ] camera
+- [ ] something like visual script
+- [ ] optimize
+
+# Ability
 - [x] move
 - [x] zoom
     - [ ] zoom to center of screen
-- [x] guild line background
+- [ ] mouse position ( by zoom )
 - [x] add item by position
     - [ ] scale
 
+# draw
+- [x] guild line background
+- [ ] positioned item
 
-- [ ] simulate geometry
-    - [ ] draw view -size -zoom
+- [ ] offset to camera ( be center )
+
+# layers controller
+    - [x] box/border
+        - [ ] background
+        - [x] world
+    - [ ] camera?!
 
  */
 
@@ -62,7 +77,7 @@ fun GraphView(
     content: @Composable() (GraphContentScope.() -> Unit) = {},
 ) {
 
-    var mousePosition by remember { mutableStateOf(Offset.Zero) }
+    var mousePositionOnBorder by remember { mutableStateOf(Offset.Zero) }
     var scale by remember { mutableStateOf(1f) }
     var offsetX by remember { mutableStateOf(0f) }
     var offsetY by remember { mutableStateOf(0f) }
@@ -75,39 +90,32 @@ fun GraphView(
 
     // Center of World:: Top/Right
     val viewState = ViewState(
-        position = Offset(-offsetXAnimate, -offsetYAnimate),
-        size = screenSize,
-        zoom = scaleAnimate
+        centerWorldOffset = Offset(-offsetXAnimate, -offsetYAnimate),
+        screenSize = screenSize,
+        showScale = scaleAnimate
     )
 
-    println(viewState)
+//    println(viewState)
 
 
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .clip(RectangleShape)
-    ) {
+    // Box/Border Layout
+    boxBorder(sizeChange = { screenSize = it }) {
+
 
         // background guild line
         // TODO: REMOVE Flicking, make exact
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .onGloballyPositioned { layoutCoordinates ->
-                    screenSize = layoutCoordinates.size
-                }
-        ) {
+        Box(modifier = Modifier.fillMaxSize()) {
 
 
             val offsetEdge = (RepeatOffset * scaleAnimate).toInt()
             val guildLineDraw = offsetEdge * 10
 
             val alpha =
-                if (viewState.zoom > 1)
+                if (viewState.showScale > 1)
                     1f
                 else
-                    (if (viewState.zoom <= .2f) 0f else viewState.zoom)
+                    (if (viewState.showScale <= .2f) 0f else viewState.showScale)
+
 
 
             Box(
@@ -129,7 +137,7 @@ fun GraphView(
                             drawLine(
                                 Color.LightGray.copy(alpha = alpha),
                                 start = Offset(x = 0f, y = y),
-                                end = Offset(x = viewState.size.width.toFloat(), y = y),
+                                end = Offset(x = viewState.screenSize.width.toFloat(), y = y),
                             )
                         }
                     }
@@ -153,7 +161,7 @@ fun GraphView(
                             drawLine(
                                 Color.LightGray.copy(alpha = alpha),
                                 start = Offset(y = 0f, x = x),
-                                end = Offset(y = viewState.size.width.toFloat(), x = x),
+                                end = Offset(y = viewState.screenSize.width.toFloat(), x = x),
                             )
                         }
                     }
@@ -177,7 +185,7 @@ fun GraphView(
                             drawLine(
                                 Color.Gray,
                                 start = Offset(x = 0f, y = y),
-                                end = Offset(x = viewState.size.width.toFloat(), y = y),
+                                end = Offset(x = viewState.screenSize.width.toFloat(), y = y),
                             )
                         }
                     }
@@ -202,7 +210,7 @@ fun GraphView(
                             drawLine(
                                 Color.Gray,
                                 start = Offset(y = 0f, x = x),
-                                end = Offset(y = viewState.size.width.toFloat(), x = x),
+                                end = Offset(y = viewState.screenSize.width.toFloat(), x = x),
                             )
                         }
                     }
@@ -230,20 +238,21 @@ fun GraphView(
 
                 }
                 .onPointerEvent(PointerEventType.Move) {
-                    mousePosition = it.changes.first().position
+                    mousePositionOnBorder = it.changes.first().position
                 }
                 .onClick {
 
+
+// TODO: set by view offset
                     val offset = Offset(
-                        x = mousePosition.x - offsetXAnimate,
-                        y = mousePosition.y - offsetYAnimate
+                        x = mousePositionOnBorder.x - offsetXAnimate,
+                        y = mousePositionOnBorder.y - offsetYAnimate
                     )
 
                     onViewClick(offset)
 
                 }
                 .fillMaxSize()
-//                .border(1.dp, Color.LightGray)
         )
 
         // Content Layout
@@ -253,10 +262,12 @@ fun GraphView(
 //                .scale(scaleAnimate)
         ) {
 
-            graphContentScope(viewState) {
+            drawLayout(viewState) {
+                graphContentLayer(viewState) {
 
-                content()
+                    content()
 
+                }
             }
 
 
@@ -320,24 +331,70 @@ fun GraphView(
 
 }
 
-data class ViewState(val position: Offset, var size: IntSize, val zoom: Float) {
+@Composable
+inline fun boxBorder(
+    crossinline sizeChange: (IntSize) -> Unit,
+    function: @Composable () -> Unit,
+) {
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .clip(RectangleShape)
+            .onGloballyPositioned { layoutCoordinates ->
+                sizeChange(layoutCoordinates.size)
+            }
+    ) {
+        function()
+    }
+}
+
+@Composable
+inline fun drawLayout(viewState: ViewState, content: @Composable () -> Unit) {
+
+    // draw Layer
+    Box(
+        Modifier.offset(viewState.worldOffsetX.dp, viewState.worldOffsetY.dp)
+    ) {
+
+        content()
+
+    }
+}
+
+//class CameraTemp(position: Offset, offset: Offset, zoom: Float) {
+//
+//}
+//
+//class ScreenTemp(val centerWorldOffset: Offset, var screenSize: IntSize) {
+//
+//}
 
 
-    val screen = Unit
-    val camera = Unit
+data class ViewState(val centerWorldOffset: Offset, var screenSize: IntSize, val showScale: Float) {
 
 
-    val viewYRange get() = (position.y..(size.height + position.y))
-    val viewYRangeInt get() = (position.y.toInt()..(size.height + position.y).toInt())
-    val viewXRange get() = (position.x..(size.width + position.x))
-    val viewXRangeInt get() = (position.x.toInt()..(size.width + position.x).toInt())
+//    val screen = ScreenTemp(centerWorldOffset, screenSize)
+//    val camera = CameraTemp(centerWorldOffset, Offset.Zero, showScale)
+
+    val worldOffsetX get() = -centerWorldOffset.x + halfScreenX
+    val worldOffsetY get() = -centerWorldOffset.y + halfScreenY
+
+    val halfScreenX get() = screenSize.width / 2
+    val halfScreenY get() = screenSize.height / 2
 
 
-    val xStartView get() = position.x
-    val xEndView get() = position.x + size.width
+    val viewYRange get() = (centerWorldOffset.y..(screenSize.height + centerWorldOffset.y))
+    val viewYRangeInt get() = ((centerWorldOffset.y - halfScreenY).toInt()..(screenSize.height - halfScreenY + centerWorldOffset.y).toInt())
+    val viewXRange get() = (centerWorldOffset.x..(screenSize.width + centerWorldOffset.x))
+    val viewXRangeInt get() = ((centerWorldOffset.x - halfScreenX).toInt()..(screenSize.width - halfScreenX + centerWorldOffset.x).toInt())
 
-    val yStartView get() = position.y
-    val yEndView get() = position.y + size.height
+
+    val xStartView get() = centerWorldOffset.x
+    val xEndView get() = centerWorldOffset.x + screenSize.width
+
+    val yStartView get() = centerWorldOffset.y
+    val yEndView get() = centerWorldOffset.y + screenSize.height
 
 }
 
@@ -374,11 +431,16 @@ class GraphContentScope(val viewState: ViewState) {
 //                    x = ((item.position.x - viewState.position.x) * viewState.zoom).dp,
 //                    y = ((item.position.y - viewState.position.y) * viewState.zoom).dp
 //                )
+//                .offset(
+//                    x = ((item.position.x * viewState.showScale) - viewState.centerWorldOffset.x + viewState.halfScreenX).dp,
+//                    y = ((item.position.y * viewState.showScale) - viewState.centerWorldOffset.y + viewState.halfScreenY).dp
+//                )
+
                 .offset(
-                    x = ((item.position.x * viewState.zoom) - viewState.position.x).dp,
-                    y = ((item.position.y * viewState.zoom) - viewState.position.y).dp
+                    x = (item.position.x * viewState.showScale).dp,
+                    y = (item.position.y * viewState.showScale).dp
                 )
-                .scale(viewState.zoom)
+                .scale(viewState.showScale)
                 .size(10.dp)
                 .background(Color.Gray)
         )
@@ -399,7 +461,7 @@ class GraphContentScope(val viewState: ViewState) {
 }
 
 @Composable
-fun graphContentScope(
+fun graphContentLayer(
     viewState: ViewState,
     function: @Composable GraphContentScope.() -> Unit,
 ) {
